4e4e026b957d5a696118174ebf86770d017d07c8
ZHadjah <zhadjah@gmail.com>
2023-01-07 20:52:44 -0500

    safety push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import "../src/styles/styles.css";
+
+import Home from "./pages/home";
+import Code from "./pages/code";
+import Solve from "./pages/solve";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Navbar githubLink='https://github.com/ZHadjah/tacocatgrammar' portfolioLink='https://zachhadjah.netlify.app/' />
+
+
+       <h1>TESTING</h1>
+       <h1>TESTING</h1>
+       <h1>TESTING</h1>
+       <h1>TESTING</h1>
+       <h1>TESTING</h1>
+
+      {/* <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/code" element={<Code />} />
+          <Route path="/solve" element={<Solve />} />
+        </Routes>
+      </div> */}
+
+
+
+
+    </>
   );
 }
-
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,71 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+function Navbar({ githubLink, portfolioLink }) {
+
+    //sticky navbar
+    // const [sticky, setSticky] = useState(false);
+  
+    // useEffect(() => {
+    //   const handleScroll = () => {
+    //     setSticky(window.scrollY > 200)
+    //   }
+    //   window.addEventListener('scroll', handleScroll)
+    //   return () => window.removeEventListener('scroll', handleScroll)
+    // })  
+  
+  
+    //hamburger icon for responsive layout
+    // const [clicked, setClicked] = useState(false);
+    // function handleClick() {
+    //   setClicked(!clicked);
+    // }
+  
+    //navbar component
+    return (
+      <nav /*id={`${sticky ? "sticky" : ""}`} */ className="nav">
+        <Link>Mortgage Manager</Link>
+  
+        <ul id="navbar" /*className={clicked ? "#navbar active" : "#navbar"}*/>
+          <CustomLink to="/">🌮Home</CustomLink>
+          <CustomLink to="/solve">🌮Solve</CustomLink>
+          <CustomLink to="/code">🌮Code</CustomLink>
+          <Link
+            id="githubLink"
+            // githubLink={githubLink}
+            // onClick={() => openInNewTab(githubLink)}
+          >
+            🌮Github
+          </Link>
+          <Link
+            id="portfolioLink"
+            // portfolioLink={portfolioLink}
+            // onClick={() => openInNewTab(portfolioLink)}
+          >
+            🌮Portfolio
+          </Link>
+        </ul>
+        <div id="mobile">
+          <i
+            id="bar"
+            // className={clicked ? "fas fa-times" : "fas fa-bars"}
+            // onClick={() => handleClick()}
+          ></i>
+        </div>
+      </nav>
+    );
+  }
+
+  const CustomLink = ({ to, children, ...props }) => {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    );
+  };
+
+  export default Navbar;
